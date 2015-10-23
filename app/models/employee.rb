4f3bc905f4e42885e5988cae17ab0819a7852f38
lucianomonaco <luciano@powertask.com.br>
2015-10-22 22:35:03 -0200

    validate phone number employees

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,6 +2,8 @@ class Employee < ActiveRecord::Base
   belongs_to :unit
   
   validates_presence_of :name, :unit_id
+  validates_format_of :phone, :with => /\A^[\d]+$\Z/, :message => " - Deve ser um número"
+
 
   def self.list(unit)
     self.where("unit_id = ?", unit).order("name ASC")
