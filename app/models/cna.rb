37a97c5da54a768db185b874ae0a4de61b1dc838
lucianomonaco <luciano@powertask.com.br>
2015-10-28 01:09:14 -0200

    cna

--- app/models/cna.rb
+++ app/models/cna.rb
@@ -4,6 +4,8 @@ class Cna < ActiveRecord::Base
   enum status: [:not_pay, :pay]
   enum stage: [:regular, :law]
 
+  usar_como_dinheiro :amount
+
   def self.list(unit)
     self.where("unit_id = ?", unit)
   end
