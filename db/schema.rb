acf29548359c203c96e8d0aaf0ddaa64dfd504cc
Marcelo Reichert <marcelo@powertask.com.br>
2015-11-26 23:16:01 -0200

    commit schema

--- db/schema.rb
+++ db/schema.rb
@@ -176,26 +176,6 @@ ActiveRecord::Schema.define(version: 20151125034235) do
 
   add_index "taxpayers", ["client_id"], name: "index_taxpayers_on_client_id", using: :btree
 
-  create_table "temp_cna", force: :cascade do |t|
-    t.integer "cod_proprietario"
-    t.string  "num_uf"
-    t.string  "exercicio"
-    t.integer "sequencial"
-    t.string  "nom_proprietario"
-    t.string  "tip_pessoa"
-    t.string  "endereco"
-    t.string  "bairro"
-    t.string  "cep"
-    t.string  "nom_municipio"
-    t.string  "sigla_uf"
-    t.string  "val_cna"
-    t.string  "val_multa_cna"
-    t.string  "val_juros_cna"
-    t.string  "total"
-    t.date    "dat_vencimento"
-    t.string  "tip_cobranca"
-  end
-
   create_table "tickets", force: :cascade do |t|
     t.integer  "unit_id"
     t.integer  "contract_id"
