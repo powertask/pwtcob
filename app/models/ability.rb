e7aedf8ca836ed19f16d5ebdd5609dc3145df509
Marcelo Reichert <marcelo@powertask.com.br>
2016-01-25 13:50:31 -0200

    Permite User entrar na tela de contribuintes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,6 +8,7 @@ class Ability
     can :manage, :all if user.admin?
 
     can :manage, Home if user.user?
+    can :manage, Taxpayer 
 
     # Define abilities for the passed in user here. For example:
     #
