a078dd5443071c076d1ea34fba2eeece6a0ba11d
Marcelo Reichert <marcelo@powertask.com.br>
2016-02-14 16:51:05 -0200

    Agenda colaborador

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,5 +1,5 @@
 class HistoriesController < ApplicationController
-  before_action :set_history, only: [:show, :edit, :update, :destroy]
+  before_action :set_city, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
   load_and_authorize_resource
   respond_to :html
@@ -7,40 +7,30 @@ class HistoriesController < ApplicationController
 
   def index
     @histories = index_class(History)
-    respond_with @histories, :layout => 'application'
+    respond_with @cities, :layout => 'application'
   end
 
-  # GET /cities/1
-  # GET /cities/1.json
   def show
   end
 
-  # GET /cities/new
   def new
     @history = History.new
     @history.unit_id = session[:unit_id]
-    @history.employee_id = session[:employee_id]
-    @history.taxpayer_id = session[:taxpayer_id]
-    @history.history_date = Time.current
   end
 
-  # GET /cities/1/edit
   def edit
   end
 
   def create
     @history = History.new(history_params)
     @history.save
-    redirect_to( show_path(session[:taxpayer_id]))
+    respond_with @history, notice: 'Histórico criado com sucesso.'
   end
 
-  # PATCH/PUT /cities/1
-  # PATCH/PUT /cities/1.json
   def update
     @city.update_attributes(city_params)
     respond_with @city, notice: 'Cidade atualizada com sucesso.'
   end
-
 
 
   private
@@ -51,6 +41,6 @@ class HistoriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def history_params
-      params.require(:history).permit(:description, :history_date, :unit_id, :taxpayer_id, :employee_id)
+      params.require(:history).permit(:name, :status, :state, :unit_id, :fl_charge)
     end
 end
