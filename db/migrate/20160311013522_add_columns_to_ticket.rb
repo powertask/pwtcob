74a65708a145d28edf9450b280223adf2092ff05
Marcelo Reichert <marcelo@powertask.com.br>
2016-03-10 22:55:06 -0300

    Status do Ticket deve ser Integer

--- db/migrate/20160311013522_add_columns_to_ticket.rb
+++ db/migrate/20160311013522_add_columns_to_ticket.rb
@@ -1,6 +1,6 @@
 class AddColumnsToTicket < ActiveRecord::Migration
   def change
-  	add_column :tickets, :status, :boolean
+  	add_column :tickets, :status, :integer
   	add_column :tickets, :paid_at, :date
   	add_column :tickets, :paid_amount, :float
   end
