d1d4745097cb02e326bc8710dad21e9026375722
Marcelo Reichert <marcelo@powertask.com.br>
2016-04-13 00:49:27 -0300

    Criar filtro para situação dos boletos emitidos

--- app/controllers/bank_billets_controller.rb
+++ app/controllers/bank_billets_controller.rb
@@ -5,14 +5,32 @@ class BankBilletsController < ApplicationController
   layout 'window'
 
   def index
-    if current_user.client?
-      @bank_billets = BankBillet.where("unit_id = ? AND bank_billet_account_id = ?", session[:unit_id], 2).order('expire_at DESC').paginate(:page => params[:page], :per_page => 20)
-    elsif current_user.user?
-      @bank_billets = BankBillet.paginate_by_sql(['select b.* from contracts c, tickets t, bank_billets b where c.id = t.contract_id and t.bank_billet_id = b.id and c.user_id = ?', current_user.id], :page => params[:page], :per_page => 20)
-    elsif current_user.admin?
-      @bank_billets = BankBillet.where("unit_id = ?", session[:unit_id]).order('expire_at DESC').paginate(:page => params[:page], :per_page => 20)
-    end
+    if params[:status].nil? or params[:status].downcase == 'todos'
+      if current_user.client?
+        @bank_billets = BankBillet.where("unit_id = ? AND bank_billet_account_id = ?", session[:unit_id], 2).order('expire_at DESC').paginate(:page => params[:page], :per_page => 20)
+      elsif current_user.user?
+        @bank_billets = BankBillet.paginate_by_sql(['select b.* from contracts c, tickets t, bank_billets b where c.id = t.contract_id and t.bank_billet_id = b.id and c.user_id = ?', current_user.id], :page => params[:page], :per_page => 20)
+      elsif current_user.admin?
+        @bank_billets = BankBillet.where("unit_id = ?", session[:unit_id]).order('expire_at DESC').paginate(:page => params[:page], :per_page => 20)
+      end
 
+    else
+      status = 0 if params[:status].downcase == 'gerando'
+      status = 1 if params[:status].downcase == 'aberto'
+      status = 2 if params[:status].downcase == 'cancelado'
+      status = 3 if params[:status].downcase == 'pago'
+      status = 4 if params[:status].downcase == 'vencido'
+      status = 5 if params[:status].downcase == 'bloqueado'
+      status = 6 if params[:status].downcase == 'devolucao'
+
+      if current_user.client?
+        @bank_billets = BankBillet.where("unit_id = ? AND bank_billet_account_id = ? AND status = ?", session[:unit_id], 2, status).order('expire_at DESC').paginate(:page => params[:page], :per_page => 20)
+      elsif current_user.user?
+        @bank_billets = BankBillet.paginate_by_sql(['select b.* from contracts c, tickets t, bank_billets b where c.id = t.contract_id and t.bank_billet_id = b.id and c.user_id = ? AND t.status = ?', current_user.id, status], :page => params[:page], :per_page => 20)
+      elsif current_user.admin?
+        @bank_billets = BankBillet.where("unit_id = ? AND status = ?", session[:unit_id], status).order('expire_at DESC').paginate(:page => params[:page], :per_page => 20)
+      end     
+    end
     respond_with @bank_billets, :layout => 'application'
   end
 
