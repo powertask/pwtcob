ee99785a7f6adcd039b38f693904211f14a68b41
Marcelo Reichert <marcelo@powertask.com.br>
2016-04-13 22:43:04 -0300

    Total situacao boletos

--- app/controllers/bank_billets_controller.rb
+++ app/controllers/bank_billets_controller.rb
@@ -5,6 +5,7 @@ class BankBilletsController < ApplicationController
   layout 'window'
 
   def index
+
     if params[:status].nil? or params[:status].downcase == 'todos'
       if current_user.client?
         @bank_billets = BankBillet.where("unit_id = ? AND bank_billet_account_id = ?", session[:unit_id], 2).order('expire_at DESC').paginate(:page => params[:page], :per_page => 20)
@@ -12,6 +13,7 @@ class BankBilletsController < ApplicationController
         @bank_billets = BankBillet.paginate_by_sql(['select b.* from contracts c, tickets t, bank_billets b where c.id = t.contract_id and t.bank_billet_id = b.id and c.user_id = ?', current_user.id], :page => params[:page], :per_page => 20)
       elsif current_user.admin?
         @bank_billets = BankBillet.where("unit_id = ?", session[:unit_id]).order('expire_at DESC').paginate(:page => params[:page], :per_page => 20)
+        status_counter
       end
 
     else
@@ -29,6 +31,7 @@ class BankBilletsController < ApplicationController
         @bank_billets = BankBillet.paginate_by_sql(['select b.* from contracts c, tickets t, bank_billets b where c.id = t.contract_id and t.bank_billet_id = b.id and c.user_id = ? AND t.status = ?', current_user.id, status], :page => params[:page], :per_page => 20)
       elsif current_user.admin?
         @bank_billets = BankBillet.where("unit_id = ? AND status = ?", session[:unit_id], status).order('expire_at DESC').paginate(:page => params[:page], :per_page => 20)
+        status_counter
       end     
     end
     respond_with @bank_billets, :layout => 'application'
@@ -57,6 +60,12 @@ class BankBilletsController < ApplicationController
   private
     def bank_billet_params
       params.require(:bank_billet).permit(:unit_id, :bank_billet_account_id, :origin_code, :our_number, :amount, :expire_at, :customer_person_name, :customer_cnpj_cpf, :status, :paid_at, :paid_amount, :shorten_url, :fine_for_delay, :late_payment_interest, :document_date, :document_amount)
+    end
+
+    def status_counter 
+      @count_open = BankBillet.opened.where("unit_id = ?", session[:unit_id]).count
+      @count_overdue = BankBillet.overdue.where("unit_id = ?", session[:unit_id]).count
+      @count_paid = BankBillet.paid.where("unit_id = ?", session[:unit_id]).count
     end
 
 end
