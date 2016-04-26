fcfa5c243a534b3886b84369b0effa63432e5627
Marcelo Reichert <marcelo@powertask.com.br>
2016-04-26 18:25:32 -0300

    Contagem mensal

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -254,14 +254,15 @@
   def contracts_meter 
     dt_ini = Date.new(Date.current.year, Date.current.month, 1).beginning_of_day
     dt_end = Date.current.end_of_day
-    @count_contracts_month = Contract.list(session[:unit_id]).where('user_id = ? and contract_date between ? AND ?', current_user.id, dt_ini, dt_end ).active.count
 
     if current_user.admin?
       @count_contracts_day        = Contract.list(session[:unit_id]).active.where('contract_date between ? AND ?', Date.current.beginning_of_day, Date.current.end_of_day).count
+      @count_contracts_month      = Contract.list(session[:unit_id]).active.where('contract_date between ? AND ?', dt_ini, dt_end ).count
       @count_contracts_day_master = Contract.list(session[:unit_id]).active.where('contract_date between ? AND ?', Date.current.beginning_of_day, Date.current.end_of_day).group('user_id').count
       @histories                  = History.list(session[:unit_id]).where('history_date is not null').order('history_date DESC').limit(15)
     else
       @count_contracts_day        = Contract.list(session[:unit_id]).active.where('user_id = ? AND contract_date between ? AND ?', current_user.id, Date.current.beginning_of_day, Date.current.end_of_day).count
+      @count_contracts_month      = Contract.list(session[:unit_id]).active.where('user_id = ? and contract_date between ? AND ?', current_user.id, dt_ini, dt_end ).count
       @count_contracts_day_master = Contract.list(session[:unit_id]).active.where('user_id = ? AND contract_date between ? AND ?', current_user.id, Date.current.beginning_of_day, Date.current.end_of_day).group('user_id').count
       @histories                  = History.list(session[:unit_id]).where('user_id = ? AND history_date is not null', current_user.id).order('history_date DESC').limit(15) if current_user.user?
     end
